6f701e738cc842bf2e32ad96ccee8107ae3f3fd4
Sean Stephens <seanastephens@email.arizona.edu>
2015-06-02 02:10:35 -0700

    Added #maize_sorghum

--- synteny-vis.js
+++ synteny-vis.js
@@ -32,6 +32,12 @@ switch (window.location.hash) {
       .defer(d3.json, 'lengths/16911.json')
       .defer(d3.json, 'lengths/3068.json');
     break;
+  case '#maize_sorghum':
+  case '#m':
+    q = q.defer(d3.json, 'data/maize_sorghum.json')
+      .defer(d3.json, 'lengths/6807.json')
+      .defer(d3.json, 'lengths/8082.json');
+    break;
   default:
     alert("Don't know what '" + window.location.hash + "' is. Loading homo_chimp.json");
     q = q.defer(d3.json, 'data/homo_chimp.json')
