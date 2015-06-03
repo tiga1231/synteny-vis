6b22d77c699203980017a6a8caf187f39d620140
Sean Stephens <seanastephens@email.arizona.edu>
2015-06-02 18:22:00 -0700

    Lots of cleanup, added brushing back in

--- synteny-vis.js
+++ synteny-vis.js
@@ -1,13 +1,8 @@
-function cumulative_counts(data) {
-  var ret = [];
-  var count = 0;
-  for (var i = 0; i < data.length; i++) {
-    ret[i] = count;
-    count += data[i];
-  }
-  ret[i] = count;
-  return ret;
-}
+var syntenyLineStrokeWidth = 3;
+var gridLineStrokeWidth = 1;
+var numHistogramTicks = 20;
+var histogramYScaleTransitionLength = 750;
+
 
 var q = queue();
 
@@ -49,50 +44,40 @@ q.await(function(error, data, aLengths, bLengths) {
     return;
   }
 
-  //Make sure we gave the length files in the right order.
+  // Sanity check
   var aID = data[0].header.aID_c.split('_')[0];
   aID = aID.substring(1, aID.length);
   var bID = data[0].header.bID_c.split('_')[0];
   bID = bID.substring(1, bID.length);
-  if (aID !== aLengths.id) {
-    console.log('You got the length files backwards, swapping...');
-    var t = aLengths;
-    aLengths = bLengths;
-    bLengths = t;
-  }
   if (aID !== aLengths.id || bID !== bLengths.id) {
-    console.log('Are you using the right length files?');
-    console.log('Length files have ID\'s: ' + aLengths.id +
-      ' and ' + bLengths.id);
+    console.log('Something went wrong:');
+    console.log('Length files have ID\'s: ' + aLengths.id + ' and ' + bLengths.id);
     console.log('Data file has ID\'s: ' + aID + ' and ' + bID);
     return;
   }
 
   // Compute cumulative BP counts
-  var xLengths = _.map(aLengths.lengths, function(d) {
-    return {
-      name: d.name,
-      length: Number(d.length)
-    };
-  });
+  function convertLengthToNumber(list) {
+    return _.map(list, function(d) {
+      d.length = Number(d.length);
+      return d;
+    });
+  }
 
-  var yLengths = _.map(bLengths.lengths, function(d) {
-    return {
-      name: d.name,
-      length: Number(d.length)
-    };
-  });
+  function cumulative_counts(data) {
+    return _.reduce(data, function(vals, d) {
+      vals.push(_.last(vals) + d);
+      return vals;
+    }, [0]);
+  }
 
-  xLengths.sort(function(a, b) {
-    return a.length < b.length ? 1 : -1;
-  });
-  yLengths.sort(function(a, b) {
-    return a.length < b.length ? 1 : -1;
-  });
+  var aNumericLengths = convertLengthToNumber(aLengths.lengths);
+  var bNumericLengths = convertLengthToNumber(bLengths.lengths);
+  var xLengths = _.sortBy(aNumericLengths, 'length').reverse();
+  var yLengths = _.sortBy(bNumericLengths, 'length').reverse();
 
   var xNames = _.pluck(xLengths, 'name');
   var yNames = _.pluck(yLengths, 'name');
-
   var xCumBPCount = cumulative_counts(_.pluck(xLengths, 'length'));
   var yCumBPCount = cumulative_counts(_.pluck(yLengths, 'length'));
 
@@ -130,22 +115,17 @@ q.await(function(error, data, aLengths, bLengths) {
 
   // Combine all chunks
   data = _.flatten(_.pluck(data, 'data'));
-  // clean data
   data = _.filter(data, function(d) {
     return isFinite(d[field]);
-  });
-  data = _.sortBy(data, function(d) {
-    return -d[field];
   });
 
   var BPPerPixel = xTotalBPs / width;
   var base = 4;
   var numLevels = Math.ceil(Math.log(BPPerPixel) / Math.log(base));
-  var levels = [];
-  for (var i = 0; i < numLevels; i++) {
-    var level = BPPerPixel / Math.pow(base, i);
-    levels.push(level);
-  }
+  var levels = _.map(_.range(numLevels), function(i) {
+    return BPPerPixel / Math.pow(base, i);
+  });
+
   var merged = merge(data, field, levels);
   var bvh_nodes = bvh_build(data);
   var prevMergeIndex = 0;
@@ -155,56 +135,69 @@ q.await(function(error, data, aLengths, bLengths) {
   var yExtent = [0, yTotalBPs];
   var xScale = d3.scale.linear().domain(xExtent).range([0, width]);
   var yScale = d3.scale.linear().domain(yExtent).range([height, 0]);
-  var xScaleOriginal = d3.scale.linear().domain(xExtent).range([0, width]);
-  var yScaleOriginal = d3.scale.linear().domain(yExtent).range([height, 0]);
+  var xScaleOriginal = xScale.copy();
+  var yScaleOriginal = yScale.copy();
 
   var zoom = d3.behavior.zoom().x(xScale).y(yScale).scaleExtent([1, 10000]).on('zoom', zoomed);
 
-  var wholePlotExtent = [
-    [0, 0],
-    [1e15, 1e15]
-  ];
+  // Can be used as array or by bvh/merge stuff
+  var wholePlot = {
+    0: [0, 0],
+    1: [1e15, 1e15],
+    xmin: 0,
+    ymin: 0,
+    xmax: 1e15,
+    ymax: 1e15
+  };
 
   function resizeBrushBoundary() {
-    var s = scaling;
-    // Fix the brush extent cursor box sizes
-    d3.select('.resize.e').select('rect')
-      .attr('width', 6 / s).attr('x', -3 / s);
-    d3.select('.resize.w').select('rect')
-      .attr('width', 6 / s).attr('x', -3 / s);
-    d3.select('.resize.n').select('rect')
-      .attr('height', 6 / s).attr('y', -3 / s);
-    d3.select('.resize.s').select('rect')
-      .attr('height', 6 / s).attr('y', -3 / s);
+    var scaling = zoom.scale();
+    var corners = ['.nw', '.ne', '.se', '.sw'];
+    var vertical = ['.e', '.w'];
+    var horizontal = ['.n', '.s'];
+    var horizontalRescale = _.union(corners, horizontal);
+    var verticalRescale = _.union(corners, vertical);
 
-    d3.select('.resize.nw').select('rect')
-      .attr('width', 6 / s).attr('x', -3 / s)
-      .attr('height', 6 / s).attr('y', -3 / s);
-    d3.select('.resize.ne').select('rect')
-      .attr('width', 6 / s).attr('x', -3 / s)
-      .attr('height', 6 / s).attr('y', -3 / s);
-    d3.select('.resize.se').select('rect')
-      .attr('width', 6 / s).attr('x', -3 / s)
-      .attr('height', 6 / s).attr('y', -3 / s);
-    d3.select('.resize.sw').select('rect')
-      .attr('width', 6 / s).attr('x', -3 / s)
-      .attr('height', 6 / s).attr('y', -3 / s);
+    _.map(horizontalRescale, function(name) {
+      d3.select('.resize' + name).select('rect')
+        .attr('width', 6 / scaling).attr('x', -3 / scaling);
+    });
+
+    _.map(verticalRescale, function(name) {
+      d3.select('.resize' + name).select('rect')
+        .attr('height', 6 / scaling).attr('y', -3 / scaling);
+    });
+  }
+
+  function mainBrush() {
+    var e = brush.extent();
+    e = [
+      [xScaleOriginal.invert(e[0][0]), yScaleOriginal.invert(e[1][1])],
+      [xScaleOriginal.invert(e[1][0]), yScaleOriginal.invert(e[0][1])],
+    ];
+    svg.selectAll('.synteny').classed('unselectedByMain', function(d) {
+      return d.xmin > e[1][0] || d.xmax < e[0][0] ||
+        d.ymin > e[1][1] || d.ymax < e[0][1];
+    });
+    console.log(e[0], e[1]);
+    updatePlot(brush.extent(), false);
+    resizeBrushBoundary();
+  }
+
+  function mainBrushEnd() {
+    if (brush.empty()) {
+      svg.selectAll('.synteny').classed('unselectedByMain', false);
+      updatePlot(wholePlot, true);
+    } else {
+      updatePlot(brush.extent(), true);
+    }
   }
 
   var brush = d3.svg.brush()
     .x(d3.scale.linear().domain([0, width]).range([0, width]))
     .y(d3.scale.linear().domain([0, height]).range([0, height]))
-    .on('brush', function() {
-      updatePlot(brush.extent(), false);
-      resizeBrushBoundary();
-    })
-    .on('brushend', function() {
-      if (brush.empty()) {
-        updatePlot(wholePlotExtent, true);
-      } else {
-        updatePlot(brush.extent(), true);
-      }
-    });
+    .on('brush', mainBrush)
+    .on('brushend', mainBrushEnd);
 
   var svgPre = d3.select('body').append('svg')
     .attr('width', width)
@@ -243,61 +236,50 @@ q.await(function(error, data, aLengths, bLengths) {
       return 'M ' + x1 + ' ' + y + ' L ' + x2 + ' ' + y;
     });
 
-  var wholePlotBBox = {
-    xmin: 0,
-    ymin: 0,
-    xmax: 1e15,
-    ymax: 1e15
-  };
 
-  var dataExtent = d3.extent(bvh_find(bvh_nodes, wholePlotBBox), function(d) { return d[field]; });
+  var dataExtent = d3.extent(bvh_find(bvh_nodes, wholePlot), function(d) {
+    return d[field];
+  });
   var colorScale = d3.scale.linear().domain(dataExtent)
     .range(["red", "green"]);
 
+  var dataSel;
+
+  function setSyntenyData(lines) {
+    var tempSel = svg.selectAll('.synteny').data(lines);
+    tempSel.enter().append('line').classed('synteny', true);
+    tempSel
+      .attr('x1', function(d) {
+        return xScaleOriginal(d.x1);
+      })
+      .attr('y1', function(d) {
+        return yScaleOriginal(d.y1);
+      })
+      .attr('x2', function(d) {
+        return xScaleOriginal(d.x2);
+      })
+      .attr('y2', function(d) {
+        return yScaleOriginal(d.y2);
+      })
+      .style('stroke', function(d) {
+        return colorScale(d.summary);
+      })
+      .classed('unselectedByMain', false)
+      .classed('unselectedByHistogram', false);
+
+    tempSel.exit().remove();
+    dataSel = tempSel;
+  }
+
   // Data
-  var dataSel = svg.selectAll('.synteny').data(data).enter()
-    .append('line')
-    .classed('synteny', true)
-    .attr('x1', function(d) {
-      return xScale(d.x1);
-    })
-    .attr('y1', function(d) {
-      return yScale(d.y1);
-    })
-    .attr('x2', function(d) {
-      return xScale(d.x2);
-    })
-    .attr('y2', function(d) {
-      return yScale(d.y2);
-    })
-    .style('stroke', function(d) {
-      return colorScale(d.summary);
-    });
+  setSyntenyData(data);
 
   svgPre
     .append('g').attr('id', 'brush-group')
     .call(brush);
 
-  var strokeWidth = 3;
-  var lastScale = strokeWidth;
-
-  var panning = false;
-  var scaling = 1;
-  var translation = [0, 0];
-
-  function zoomed() {
-    var t = d3.event.translate;
-    var s = d3.event.scale;
-
-    var modifiedXExtent = xScale.domain();
-    var modifiedYExtent = yScale.domain();
-    var screenWidth = modifiedXExtent[1] - modifiedXExtent[0]; // Ratio is same using x or y
-    var original = xExtent[1] - xExtent[0];
-    var plotScaling = strokeWidth * screenWidth / original;
-    var gridScaling = screenWidth / original;
-
-
-    var BP = BPPerPixel * gridScaling;
+  function updateMergeLevel(s) {
+    var BP = BPPerPixel * s;
     var mergeIndex = 0;
     for (var i = levels.length - 1; i >= 0; i--) {
       if (BP < levels[i]) {
@@ -308,95 +290,56 @@ q.await(function(error, data, aLengths, bLengths) {
     if (mergeIndex !== prevMergeIndex) {
       prevMergeIndex = mergeIndex;
       data = merged[mergeIndex].sets;
-      var tempSel = svg.selectAll('.synteny').data(data);
+      setSyntenyData(data);
 
-      tempSel.enter().append('line').classed('synteny', true);
+      // Update brushes
+      plotBrushBrush();
+      mainBrush();
 
-      tempSel
-        .attr('x1', function(d) {
-          return xScaleOriginal(d.x1);
-        })
-        .attr('y1', function(d) {
-          return yScaleOriginal(d.y1);
-        })
-        .attr('x2', function(d) {
-          return xScaleOriginal(d.x2);
-        })
-        .attr('y2', function(d) {
-          return yScaleOriginal(d.y2);
-        })
-        .style('stroke', function(d) {
-          return colorScale(d.summary);
-        });
-
-      tempSel.exit().remove();
-
-      dataSel = tempSel;
+      return true;
     }
-
-
-    t[0] = Math.min(0, Math.max(-width * s + width, t[0]));
-    t[1] = Math.min(0, Math.max(-height * s + height, t[1]));
-    translation = t;
-    scaling = s;
-    zoom.translate(t); // prevents the translate from growing large. This way, you don't have to "scroll back" onto the canvas if you go too far.
-
-    d3.select('#brush-group').attr("transform", "translate(" + t + ")scale(" + d3.event.scale + ")");
-    d3.select('#data-group').attr("transform", "translate(" + t + ")scale(" + d3.event.scale + ")");
-
-
-
-    /* 
-     * Two optimizations for semantic zooming:
-     *  - Only update the stroke-width if there is a large difference
-     *    between current width and ideal width.
-     *    This lowers the number of updates that are necessary at low
-     *    magnification, which are the most expensive because...
-     *  - We only update the svg elements that are visible.
-     *
-     * The parameter k controls how extreme the first optimization is.
-     * k = 1 means that 'large' means roughly an integer difference,
-     * k = 2 means that 'large' means roughly a half integer difference, etc.
-     * (I don't know what I am talking about here.)
-     * Bigger k, smoother. Smaller k, faster.
-     */
-    var k = 3;
-    if (Math.round(k / scaling) !== Math.round(k / lastScale)) {
-      lastScale = plotScaling;
-      var xMax = modifiedXExtent[1];
-      var xMin = modifiedXExtent[0];
-      var yMax = modifiedYExtent[1];
-      var yMin = modifiedYExtent[0];
-      var wMargin = 1e7; // fudge by a little bit to catch data that starts off screen and extends on screen
-      dataSel
-        .filter(function(d) {
-          return d.x1 < xMax + wMargin && d.x1 > xMin - wMargin && d.y1 < yMax + wMargin && d.y1 > yMin - wMargin;
-        })
-        .style('stroke-width', plotScaling);
-    }
-
-    // There are so few grid lines, we can afford to update all of them
-    // all of the time.
-    d3.selectAll('.grid-horizontal').style('stroke-width', gridScaling);
-    d3.selectAll('.grid-vertical').style('stroke-width', gridScaling);
+    return false;
   }
 
-  var numTicks = 20;
-  var margin = 50;
+  var lastScale = 1;
 
-  // What a mess... d3.scale.linear.ticks decides how many ticks you get.
-  // The param is only a hint. We need to get the right number of bars, 
-  // so we make an axis just for that purpose. slice(1) deals with the 
-  // fact that the histogram layout bins between the tick values: n ticks 
-  // goes to n - 1 bins
-  var tempScale = d3.scale.linear()
-    .domain(dataExtent)
-    .range([margin, plotWidth - margin]);
-  plot.selectAll('.dataBars')
-    .data(tempScale.ticks(numTicks).slice(1))
-    .enter()
-    .append('rect').classed('dataBars', true);
-  computeBins(bvh_nodes, field, tempScale.ticks(numTicks));
+  function zoomed() {
+    var t = d3.event.translate;
+    var s = d3.event.scale;
+    t[0] = Math.min(0, Math.max(-width * s + width, t[0]));
+    t[1] = Math.min(0, Math.max(-height * s + height, t[1]));
+    // prevents the translate from growing large. This way, you don't 
+    // have to "scroll back" onto the canvas if you pan past the edge.
+    zoom.translate(t);
+
+    d3.select('#brush-group')
+      .attr("transform", "translate(" + t + ")scale(" + s + ")");
+    d3.select('#data-group')
+      .attr("transform", "translate(" + t + ")scale(" + s + ")");
+
+    var mergeUpdate = updateMergeLevel(1 / s);
+
+    /* 
+     * We only update the svg elements that are visible.
+     */
+    lastScale = s;
+    var xMax = xScale.domain()[1];
+    var xMin = xScale.domain()[0];
+    var yMax = yScale.domain()[1];
+    var yMin = yScale.domain()[0];
+
+    dataSel
+      .filter(function(d) {
+        return !(d.xmin > xMax || xMin > d.xmax ||
+          d.ymin > yMax || yMin > d.ymin);
+      })
+      .style('stroke-width', syntenyLineStrokeWidth / s);
+
+    svg.selectAll('.grid-horizontal')
+      .style('stroke-width', gridLineStrokeWidth / s);
+    svg.selectAll('.grid-vertical')
+      .style('stroke-width', gridLineStrokeWidth / s);
+  }
 
   plot.append('text')
     .attr('x', 2 * plotHeight / 3)
@@ -407,27 +350,36 @@ q.await(function(error, data, aLengths, bLengths) {
     .text(field);
 
   function plotBrushBrush() {
+    if (plotBrush.empty()) return;
     var e = plotBrush.extent();
-    // Rounding to include entire histogram bars at once
-    var min = Math.floor(numTicks * e[0]) / numTicks;
-    var max = Math.ceil(numTicks * e[1]) / numTicks;
-    svg.selectAll('.synteny').classed('selected', function(d) {
-      return d[field] <= max && d[field] >= min;
-    });
+    svg.selectAll('.synteny')
+      .classed('unselectedByHistogram', function(d) {
+        return d.summary > e[1] || d.summary < e[0];
+      });
   }
 
   function plotBrushEnd() {
     if (plotBrush.empty()) {
-      svg.selectAll('.synteny').classed('selected', false);
-      //   plot.selectAll('.dataBars').attr('fill', 'steelblue');
+      svg.selectAll('.synteny').classed('unselectedByHistogram', false);
     }
   }
 
-  var lastYExtent = [0, bvh_nodes.data.length]; // Default--no reason why
   var plotBrush = d3.svg.brush()
     .on('brush', plotBrushBrush)
     .on('brushend', plotBrushEnd);
-  var xPlotScale, yPlotScale;
+
+  var margin = 50;
+  var lastYExtent = [0, bvh_nodes.data.length];
+  var xPlotScale = d3.scale.linear()
+    .domain(dataExtent)
+    .range([margin, plotWidth - margin]);
+  var yPlotScale;
+
+  plot.selectAll('.dataBars')
+    .data(xPlotScale.ticks(numHistogramTicks).slice(1))
+    .enter()
+    .append('rect').classed('dataBars', true);
+  computeBins(bvh_nodes, field, xPlotScale.ticks(numHistogramTicks));
 
   function updatePlot(extent, shouldRescaleYAxis) {
     var e = extent;
@@ -492,15 +444,16 @@ q.await(function(error, data, aLengths, bLengths) {
       .classed('yAxis', true).call(yAxis);
 
     if (shouldRescaleYAxis) {
-      var transitionLength = 750;
-
       lastYExtent = [0, 3 / 2 * d3.max(_.pluck(filteredData, 'y'))];
       yPlotScale = d3.scale.linear()
         .domain(lastYExtent)
         .range([plotHeight - margin, margin]);
       yAxis.scale(yPlotScale);
-      yAxisSel.transition().duration(transitionLength).call(yAxis);
-      plot.selectAll('.dataBars').transition().duration(transitionLength)
+      yAxisSel.transition()
+        .duration(histogramYScaleTransitionLength)
+        .call(yAxis);
+      plot.selectAll('.dataBars').transition()
+        .duration(histogramYScaleTransitionLength)
         .attr('x', function(d) {
           return xPlotScale(d.x);
         })
@@ -516,19 +469,17 @@ q.await(function(error, data, aLengths, bLengths) {
     }
   }
 
-  updatePlot(wholePlotExtent, true); // Initialize histogram
+  updatePlot(wholePlot, true); // Initialize histogram
 
   // zoom/pan switching
   d3.selectAll("#mouse-options input[name=mouse-options]")
     .on("change", function() {
       if (this.value === 'pan') {
-        panning = true;
         d3.select('#brush-group').on('mousedown.brush', null);
         d3.select('#zoom-group').call(zoom);
         d3.select('#brush-group').style('pointer-events', null);
         d3.select('#zoom-group').style('pointer-events', 'all');
       } else if (this.value === 'brush') {
-        panning = false;
         d3.select('#brush-group').call(brush);
         d3.select('#brush-group').style('pointer-events', 'all');
         d3.select('#zoom-group').on('mousedown.zoom', null);
