ccad439730a080ee4cc65ab1e8007450cafe7110
Sean Stephens <seanastephens@email.arizona.edu>
2015-07-21 11:48:30 -0700

    Added duplicate logging

--- simplify/remove_duplicates.py
+++ simplify/remove_duplicates.py
@@ -1,13 +1,24 @@
 import sys
 
 def main(args):
-    seen = set()
-    for line in sys.stdin:
+    f = open('dups.txt', 'w')
+    seen = {}
+    for i, line in enumerate(sys.stdin):
         fields = line.strip().split(',')
         rep = tuple(fields[4:6]) + tuple(fields[16:18])
-        if rep in seen:
+        if rep in seen.keys():
+            f.write('Line ' + str(i) + ' matched line ' + str(seen[rep][0]) + '\n')
+            if seen[rep][1] == line:
+                f.write('It was an exact match.\n');
+            else:
+                x = line.strip().split(',');
+                y = seen[rep][1].strip().split(',');
+                diffs = {i for i in range(len(x)) if x[i] != y[i]}
+                f.write('They differed only in fields: ' + ','.join([str(i) for i in diffs]) + '\n')
+            f.write(line)
+            f.write(seen[rep][1])
             continue
-        seen.add(rep)
+        seen[rep] = (i, line)
         print(line, end='')
 
 
