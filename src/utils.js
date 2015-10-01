918cd73d5c0e0e8b7a5698e13668ad054cce50d0
Sean Stephens <seanastephens@email.arizona.edu>
2015-10-01 12:03:26 -0700

    Clean

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-exports.getComputedAttr = function getComputedAttr(element, attr) {
-	var computed = getComputedStyle(element)[attr];
-	return parseInt(computed, 10);
-};
-
-exports.translate = function translate(x, y) {
-	return 'translate(' + x + ',' + y + ')';
-};
-
