1d77af71b43f58fe474217a5d79ce17f7e49dd02
Sean Stephens <seanastephens@email.arizona.edu>
2015-10-29 14:32:22 -0700

    Use Array.prototype.join instead of reduce

--- src/generate-css.js
+++ src/generate-css.js
@@ -61,5 +61,5 @@ const lines = [
 	'}'
 ];
 
-const cssString = _.reduce(lines, (a, b) => a + '\n' + b);
+const cssString = lines.join('\n');
 exports.getStyleSheetForDiv = (name) => cssString.replace(/{name}/g, name);
