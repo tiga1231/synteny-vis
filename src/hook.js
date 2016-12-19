5694bfab159f4a4232990a4f0db2adf081a8aa0a
Sean Stephens <seanastephens@email.arizona.edu>
2016-12-19 15:15:21 -0700

    Clean out new branch

--- src/hook.js
+++
@@ -1,6 +0,0 @@
-import 'babel-polyfill';
-import { makeSyntenyDotPlot } from './main';
-
-// Expose our API, but not anywhere close to anything that might need to be
-// tested in node land.
-window.makeSyntenyDotPlot = makeSyntenyDotPlot;
